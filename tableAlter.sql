201f7ef0f37592a82f9600f7d3113295992241bf
coy123 <canyil@gmail.com>
2020-06-19 19:01:04 +0200

    update table creation script

--- tableAlter.sql
+++ tableAlter.sql
@@ -1,16 +1,19 @@
 #this is altering the tables according to new updates(harvestDate), you can safely delete this after the update done
 
-ALTER TABLE stormychecker.urls
-  ADD harvestDate DATE;
+#harvestdate for delete mechanism
+ALTER TABLE stormychecker_dev.urls
+  ADD harvestDate BIGINT DEFAULT 1000;#this default is there so it doesnt stay null and can be effectively used when calling deleteOldLinks method
 
-ALTER TABLE stormychecker.status
-	DROP CONSTRAINT status_ibfk_1;
-
-ALTER TABLE stormychecker.status 
+#to add on delete cascade to status
+ALTER TABLE stormychecker_dev.status
+	DROP FOREIGN KEY status_ibfk_1;
+ALTER TABLE stormychecker_dev.status 
   ADD CONSTRAINT status_ibfk_1 
   FOREIGN KEY (url) 
   REFERENCES urls(url) 
   ON DELETE CASCADE
   ON UPDATE CASCADE;
 
+
+#TODO index on harvestDate
 
