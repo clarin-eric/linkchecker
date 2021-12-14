23edb47c63a436897f6e212c525866ef18dfb4bf
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2021-12-14 14:04:58 +0100

    custom exception obsolete

--- src/main/java/at/ac/oeaw/acdh/linkchecker/exception/DeniedByRobotsException.java
+++
@@ -1,8 +0,0 @@
-package at.ac.oeaw.acdh.linkchecker.exception;
-
-public class DeniedByRobotsException extends Exception {
-
-    public DeniedByRobotsException(String message){
-        super(message);
-    }
-}
--- src/main/java/at/ac/oeaw/acdh/linkchecker/exception/LoginPageException.java
+++
@@ -1,7 +0,0 @@
-package at.ac.oeaw.acdh.linkchecker.exception;
-
-public class LoginPageException extends Exception {
-    public LoginPageException(String message) {
-        super(message);
-    }
-}
