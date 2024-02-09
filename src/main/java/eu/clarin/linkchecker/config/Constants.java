a66f91c16234ee6f2fe81fcf80eff5a1a726099f
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2024-02-09 11:32:34 +0100

    typo

--- src/main/java/eu/clarin/linkchecker/config/Constants.java
+++ src/main/java/eu/clarin/linkchecker/config/Constants.java
@@ -19,8 +19,6 @@
 package eu.clarin.linkchecker.config;
 
 public class Constants {
-    public static final String SPOUT_MAXRESULTS_PARAM_NAME = "spout.max.results";
-    public static final String SPOUT_GROUP_MAXRESULTS_PARAM_NAME = "spout.group.max.results";
     public static final String HTTP_REDIRECT_LIMIT = "http.redirectLimit";
     
     public static final String RedirectStreamName = "redirect";
@@ -28,7 +26,7 @@ public class Constants {
     public static final String LOGIN_LIST_URL = "login.list.url";
     public static final String OK_STATUS_CODES = "ok.status.codes";
     public static final String REDIRECT_STATUS_CODES = "redirect.status.codes";
-    public static final String UNDETERMINED_STATUS_CODES = "undeterminded.status.codes";
+    public static final String UNDETERMINED_STATUS_CODES = "undetermined.status.codes";
     public static final String RESTRICTED_ACCESS_STATUS_CODES = "restricted.access.status.codes";
 
     private Constants() {
