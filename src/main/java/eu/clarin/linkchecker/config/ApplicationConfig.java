3fa6f2e83f4158bc3541596f5cdbbc934386adba
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2026-08-30 18:08:28 +0200

    adding missing component scan

--- src/main/java/eu/clarin/linkchecker/config/ApplicationConfig.java
+++ src/main/java/eu/clarin/linkchecker/config/ApplicationConfig.java
@@ -2,17 +2,18 @@ package eu.clarin.linkchecker.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
-
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.hibernate.LocalSessionFactoryBean;
 
-@ComponentScan(basePackages = {"eu.clarin.linkchecker.persistence"})
-@EnableJpaRepositories("eu.clarin.linkchecker.persistence.repository")
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.config.BootstrapMode;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 @EntityScan("eu.clarin.linkchecker.persistence.model")
+@EnableJpaRepositories("eu.clarin.linkchecker.persistence.repository")
+@ComponentScan({"eu.clarin.linkchecker.persistence.service", "eu.clarin.linkchecker.persistence.generic"})
+@EnableTransactionManagement
 @EnableAutoConfiguration
 @Configuration
 public class ApplicationConfig {
