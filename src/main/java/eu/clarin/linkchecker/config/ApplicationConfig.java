d28780a895268c7592225eb195a138d2ada45357
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2026-08-30 21:42:27 +0200

    issue #92

--- src/main/java/eu/clarin/linkchecker/config/ApplicationConfig.java
+++ src/main/java/eu/clarin/linkchecker/config/ApplicationConfig.java
@@ -2,19 +2,24 @@ package eu.clarin.linkchecker.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
+
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.repository.config.BootstrapMode;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@Configuration
+@EnableAutoConfiguration
+@Import({
+        org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration.class,
+        org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration.class
+})
 @EntityScan("eu.clarin.linkchecker.persistence.model")
 @EnableJpaRepositories("eu.clarin.linkchecker.persistence.repository")
 @ComponentScan({"eu.clarin.linkchecker.persistence.service", "eu.clarin.linkchecker.persistence.generic"})
 @EnableTransactionManagement
-@EnableAutoConfiguration
-@Configuration
 public class ApplicationConfig {
 }
